945746fd9eb0d16d1a26c777972629860c696200
Gavin Bauer <gavinb@impro.net>
2019-11-18 10:10:40 +0200

    Update filegen.c

--- Files/filegen.c
+++ Files/filegen.c
@@ -3,5 +3,19 @@
 #include <string.h>
 
 int main(int argc , char *argv[]) {
+	FILE *OutFile;
+	unsigned long count;
+
+	OutFile = fopen("TestFile.bin","w");
+	if (OutFile){
+		printf("writing file\n");
+		for (count = 0; count < 0x6000000; count++)
+		{
+			fputc(0x00,OutFile);
+		}
+		fclose(OutFile);
+	}
+	else
+		printf("Failed to create output file\n");
 	return 0;
 }
